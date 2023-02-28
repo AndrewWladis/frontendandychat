229480d639f2c69e3502854b79a70f23c09eda2e
Andrew Wladis <andrewwladis@Andrews-MacBook-Pro.local>
2023-02-28 09:13:38 -0500

    fixed spam filter

--- script.js
+++ script.js
@@ -58,9 +58,11 @@ function appendMessage(message) {
   const messageElement = document.createElement('div')
   if (canType && message != 'null left') {
     messageElement.innerText = message;
-    if (message.startsWith("You:")){
+    if (message.startsWith("You")){
       messageElement.style.backgroundColor = '#3f6296'
-      setInterval(function () {canType = true}, 3000);
+      if (message.startsWith("You:")){
+        setInterval(function () {canType = true}, 3000);
+      }
     } else if (message == 'No messages can be over 150 characters!'){
       messageElement.style.backgroundColor = '#3f6296'
       messageElement.style.color = '#f79494';
